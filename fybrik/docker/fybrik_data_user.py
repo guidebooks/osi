4e862767982e14dddd593549c901912fb0e18bae
Atin Sood <atinsood@gmail.com>
2022-02-11 22:14:34 -0500

    update fybrik_data_user.py to latest

--- fybrik/docker/fybrik_data_user.py
+++ fybrik/docker/fybrik_data_user.py
@@ -6,14 +6,14 @@ import pandas as pd
 import os
 import sys
 
-def get_data(endpoint):
+def get_data(endpoint, namespace):
     # Create a Flight client
     client = fl.connect(endpoint)
     print('Getting data for endpoint '+endpoint)
 
     # Prepare the request
     request = {
-        "asset": "fybrik-notebook-sample/paysim-csv",
+        "asset": namespace+"/paysim-csv",
         # To request specific columns add to the request a "columns" key with a list of column names
         # "columns": [...]
     }
@@ -25,7 +25,7 @@ def get_data(endpoint):
     print(df)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print('Not enough arguments provided')
     else:
-        get_data(sys.argv[1])
+        get_data(sys.argv[1], sys.argv[2])
